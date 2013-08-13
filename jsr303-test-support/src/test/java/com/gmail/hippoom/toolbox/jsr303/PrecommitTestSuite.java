389c272f27e506644dd6c73a31178420c784138e
hippoom <hippoom@gmail.com>
2013-08-13 16:05:57 +0800

    1. First commit

---
+++ jsr303-test-support/src/test/java/com/gmail/hippoom/toolbox/jsr303/PrecommitTestSuite.java
@@ -0,0 +1,10 @@
+package com.gmail.hippoom.toolbox.jsr303;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({})
+public class PrecommitTestSuite {
+
+}
